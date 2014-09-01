9c13ab9fde50456830313e3e31fd0aa18c00051a
weibaohui <zihuxinyu@163.com>
2014-09-01 15:22:51 +0800

    增加用量重置功能

--- src/ShadowManage/main.go
+++ src/ShadowManage/main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"github.com/astaxie/beego"
 	_ "ShadowManage/routers"
-	//. "github.com/zihuxinyu/GoLibrary"
+	. "github.com/zihuxinyu/GoLibrary"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
 	beego.SessionOn = true
 
 
-//	white := []string{
-//		"/login",
-//		"/redis/sendemail",
-//		"/redis/index",
-//		"/login/index",
-//	}
+	white := []string{
+		"/login",
+		"/redis/sendemail",
+		"/login/index",
+	}
 
 
-//	var FilterUser = func(ctx *context.Context) {
-//		beego.Debug("ddd", ctx.Request.RequestURI)
-//		value := ctx.Input.Session("User")
-//		if value != "Manager" && !StringsContains(white, ctx.Request.RequestURI) {
-//			ctx.Redirect(302, "/login/index")
-//		}
-//	}
-//	beego.InsertFilter("/redis/*", beego.BeforeExec, FilterUser)
+	var FilterUser = func(ctx *context.Context) {
+		beego.Debug("ddd", ctx.Request.RequestURI)
+		value := ctx.Input.Session("User")
+		if value != "Manager" && !StringsContains(white, ctx.Request.RequestURI) {
+			ctx.Redirect(302, "/login/index")
+		}
+	}
+	beego.InsertFilter("/redis/*", beego.BeforeExec, FilterUser)
 
 	beego.Run()
 }
